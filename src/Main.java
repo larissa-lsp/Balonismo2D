5cba435903f2d9595171c2ab736b6cac4acbcc7d
Larissa Paixão <larissapaixaolsp@gmail.com>
2023-06-15 12:12:26 -0300

    Inicio

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+
+        Pessoa p2 = new Pessoa();
+
+        p1.nome = "Elane";
+        p2.nome = "Matteo";
+
+        p1.rua = "Rua Di Cavalcanti";
+        p1.cidade = "Barueri";
+        p1.estado = "São Paulo";
+
+        p2.rua = "Rua Di Cavalcanti";
+        p2.cidade = "Barueri";
+        p2.estado = "São Paulo";
+    }
+}
